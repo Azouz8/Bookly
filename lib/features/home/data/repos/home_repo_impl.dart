0a71419b7c0cb89d42ba60c8f62fce2f080b2e67
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2024-07-16 14:00:41 +0300

    Implement Fetch Newest Books

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/BookModel.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
+    try{
+      var data = await apiService.get(
+          endPoint:
+          "volumes?q=subject:Programming&Filtering=free-ebooks&Sorting:newest");
+      List<BookModel> books =[];
+      for(var i in data["items"]){
+        books.add(i);
+      }
+      return right(books);
+    }on Exception catch(e){
+      return left(ServerFailure());
+    }
+
   }
 
   @override
@@ -15,5 +31,4 @@ class HomeRepoImpl implements HomeRepo{
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
-
 }
