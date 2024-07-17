4413c66dfdf7aa43b862327e160c080ec129a2b2
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2024-07-17 20:23:35 +0300

    Create Featured Books Cubit States

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,16 @@
+import 'package:bookly/features/home/data/models/book_model/BookModel.dart';
+
+class FeaturedBooksState {}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+class FeaturedBooksLoading extends FeaturedBooksState{}
+class FeaturedBooksSuccess extends FeaturedBooksState{
+  List<BookModel> books;
+  FeaturedBooksSuccess(this.books);
+}
+class FeaturedBooksFail extends FeaturedBooksState{
+  String e;
+  FeaturedBooksFail(this.e){
+    print(e.toString());
+  }
+}
