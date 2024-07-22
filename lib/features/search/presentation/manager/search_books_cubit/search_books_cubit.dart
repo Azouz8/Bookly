15b2f8bd427b3f134ee9a78f9c78f1061d1e6ada
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2024-07-22 19:01:08 +0300

    Search Cubit Done

--- lib/features/search/presentation/manager/search_books_cubit/search_books_cubit.dart
+++ lib/features/search/presentation/manager/search_books_cubit/search_books_cubit.dart
@@ -1,6 +1,21 @@
+import 'package:bookly/features/search/data/repos/search_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'search_books_state.dart';
 
 class SearchBooksCubit extends Cubit<SearchBooksState> {
-  SearchBooksCubit() : super(SearchBooksInitial());
+  SearchBooksCubit({required this.searchRepo, required this.search})
+      : super(SearchBooksInitial());
+  SearchRepo searchRepo;
+  final String search;
+
+  Future<void> fetchSearchedBooks() async {
+    emit(SearchBooksLoading());
+    var result = await searchRepo.fetchSearchedBooks(search);
+    result.fold((failure) {
+      emit(SearchBooksFailure(failure as String));
+    }, (books) {
+      emit(SearchBooksSuccess(books));
+    });
+  }
 }
